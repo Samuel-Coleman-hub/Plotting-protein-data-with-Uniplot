362a36b4976eb4f4765854fe07dd9781ed8ff412
b9017517 <s.r.coleman1@newcastle.ac.uk>
2019-10-16 10:59:21 +0100

    Created a new project and copied code from the previous so that Biopython would work.

---
+++ LICENSE.md
@@ -0,0 +1,1 @@
+This project is modified by Samuel Coleman, from a practical by Phillip Lord, Copyright, 2019, Newcastle University
---
+++ uniplot.py
@@ -0,0 +1,8 @@
+import gzip
+
+from Bio import SeqIO
+
+handle = gzip.open("uniprot_receptor.xml.gz")
+for record in SeqIO.parse(handle, "uniprot-xml"):
+    print(record)
+
