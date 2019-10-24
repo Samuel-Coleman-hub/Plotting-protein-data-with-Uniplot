adcd7e1bd470b320da54a3872143acbd668edf3d
b9017517 <s.r.coleman1@newcastle.ac.uk>
2019-10-24 10:49:05 +0100

    Added command line functionality to print out both a formatted and unformatted list of the proteins to the terminal.

--- uniplot/cli.py
+++ uniplot/cli.py
@@ -1,11 +1,31 @@
 from Bio import SeqIO
 import gzip
+import argparse
 
-
-def cli():
+def dump(args):
     handle = gzip.open("uniprot_receptor.xml.gz")
     for record in SeqIO.parse(handle, "uniprot-xml"):
         print(record)
 
+def names(args):
+    handle = gzip.open("uniprot_receptor.xml.gz")
+    for record in SeqIO.parse(handle, "uniprot-xml"):
+        print(record.name)
+
+def cli():
+    ## Create a new parser
+    parser = argparse.ArgumentParser(prog="uniplot")
+
+    subparsers = parser.add_subparsers(help="Sub Command Help")
+
+    ## Add subparsers
+    subparsers.add_parser("dump").set_defaults(func=dump)
+    subparsers.add_parser("list").set_defaults(func=names)
+
+    ## Parse the command line
+    args = parser.parse_args()
+
+    ## Take the func argument, which points to our function and call it.
+    args.func(args)
 
 
